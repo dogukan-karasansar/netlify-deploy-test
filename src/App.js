6b5fec1fdaef0927c0c1b876dc403f42441d1f2d
karasansar <55123100+dogukan-karasansar@users.noreply.github.com>
2021-08-27 02:33:36 +0300

    test tested

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>netlify deploy test</h1>
+      <h1>netlify test deploy test</h1>
       <Contacts />
     </div>
   );
