c87c2d41409c95941694611d6a0dadfe3ee41400
karasansar <55123100+dogukan-karasansar@users.noreply.github.com>
2021-08-27 02:41:29 +0300

    env configure

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <h1>netlify test deploy test</h1>
       <a href="/#">learn react</a>
+      {/* {process.env.REACT_APP_API_ENDPOINT ile axios url kısmında kullanılabilir} */}
       <Contacts />
     </div>
   );
